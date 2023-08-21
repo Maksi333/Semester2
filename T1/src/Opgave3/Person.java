2c52e8548d9a3d8135ed048da40cbe94c5d1857e
Maksi333 <Simon_n_andersen@live.dk>
2023-08-21 09:38:37 +0200

    Initial Commit

---
+++ T1/src/Opgave3/Person.java
@@ -0,0 +1,11 @@
+package Opgave3;
+
+public class Person {
+    public static String name;
+    public static String adress;
+
+    public Person(String name, String adress) {
+        this.name = name;
+        this.adress = adress;
+    }
+}
