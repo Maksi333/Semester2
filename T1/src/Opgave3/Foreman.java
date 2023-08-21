88cc89295a6dc7ec6af0bd64cb98daa7036d865b
Maksi333 <Simon_n_andersen@live.dk>
2023-08-21 10:56:33 +0200

    Foreman and Mechanic

--- T1/src/Opgave3/Foreman.java
+++ T1/src/Opgave3/Foreman.java
@@ -1,4 +1,12 @@
 package Opgave3;
 
-public class Foreman {
+public class Foreman extends Person {
+    public double tillæg;
+    public int årForUdnævnelse;
+
+    public Foreman(double tillæg, int årForUdnævnelse){
+        super(name,adress);
+        this.tillæg = tillæg;
+        this.årForUdnævnelse = årForUdnævnelse;
+    }
 }
--- T1/src/Opgave3/Mechanic.java
+++ T1/src/Opgave3/Mechanic.java
@@ -1,4 +1,12 @@
 package Opgave3;
 
-public class Mechanic {
+public class Mechanic extends Person {
+    public int svendePrøveÅr;
+    public double timeLøn;
+
+    public Mechanic(int svendePrøveÅr, double timeLøn){
+        super(name, adress);
+        this.timeLøn = timeLøn;
+        this.svendePrøveÅr = svendePrøveÅr;
+    }
 }
