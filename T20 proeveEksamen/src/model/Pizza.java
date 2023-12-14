b29e124db818835bbcb9dde9b1abfe45623b535a
Maksi333 <Simon_n_andersen@live.dk>
2023-12-14 10:51:11 +0100

    nye ting

---
+++ T20 proeveEksamen/src/model/Pizza.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Pizza {
+    private String name;
+    private double price;
+
+    public Pizza(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
