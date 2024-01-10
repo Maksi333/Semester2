10bda50e89c8b09e33efbc30d271e068ffa4c6af
Maksi333 <Simon_n_andersen@live.dk>
2024-01-10 21:17:22 +0100

    nye ting

--- T20 proeveEksamen/src/controller/Controller.java
+++ T20 proeveEksamen/src/controller/Controller.java
@@ -1,5 +1,17 @@
 package controller;
 
-public class Controller {
+import model.Pizza;
+import storage.ListStorage;
+
+import java.util.ArrayList;
+
+public abstract class Controller {
+    private ListStorage storage = new ListStorage();
+
+    public Pizza createPizza(String name, double price){
+        Pizza pizza = new Pizza(name,price);
+        storage.storePizza(pizza);
+        return pizza;
+    }
 
 }
--- T20 proeveEksamen/src/gui/PizzaWindow.java
+++ T20 proeveEksamen/src/gui/PizzaWindow.java
@@ -3,11 +3,18 @@ package gui;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import model.Pizza;
 
 public class PizzaWindow extends Application {
+
+	private ListView<Pizza> lwPizzas = new ListView<>();
+	private Stage stage = new Stage();
+
 
 	@Override
 	public void start(Stage stage) {
@@ -28,6 +35,11 @@ public class PizzaWindow extends Application {
 
 		Label lblComp = new Label("Liste over pizzaer");
 		pane.add(lblComp, 0, 0);
+
+		pane.add(lwPizzas,0,1);
+		Button btnCreateNewPizza = new Button("Lav ny pizza");
+		btnCreateNewPizza.setOnAction(actionEvent -> new PizzaInputWindow("opret pizza",null).showAndWait());
+		pane.add(btnCreateNewPizza,0,2);
 
 	}
 }
